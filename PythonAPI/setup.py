c1bfa56b2b0b1b18ce293fbbf2c28680f3be2d4e
martinlightmatter <martinlightmatter@users.noreply.github.com>
2020-01-17 16:09:21 -0500

    Delay numpy import in setup.py
    distutils does not offer good support for build-time dependencies,
    despite the option of "setup_requires". This change proposes one
    (admittedly hacky) method of delaying the import of numpy until after
    the setup requirements have been processed. pycocotools having a
    build-time dependency on numpy that is not automatically handled
    by a python package manager makes the PythonAPI difficult to
    properly include as a dependency in other projects.
    
    Alternative solutions might include:
    1. use of a pyproject.toml [PEP-518](https://www.python.org/dev/peps/pep-0518/)
       to specify the build system, but this likely poses challenges for Python 2.7
       support.
    2. build_ext overloading as done in SciPy: https://github.com/scipy/scipy/blob/master/setup.py#L205
       however, this is likely to run into issues since Cython also
       customizes build_ext.
    
    If this solution is deemed too hacky, it would be helpful to
    add a clear README that specifies that numpy is a build-time
    dependency.

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,27 +1,53 @@
+from distutils.command.build import build
 from setuptools import setup, Extension
-import numpy as np
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
 
-ext_modules = [
-    Extension(
-        'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
-    )
-]
+
+#: Hack to delay numpy import until after setup_requires
+#:
+#: https://stackoverflow.com/a/54128391
+#:
+class MyBuild(build):
+
+    def finalize_options(self):
+        super().finalize_options()
+
+        # Make NumPy think that it is not in its setup process
+        __builtins__.__NUMPY_SETUP__ = False
+
+        import numpy
+
+        extension = next(
+            m for m in self.distribution.ext_modules if m == pycocotools_mask
+        )
+        extension.include_dirs.append(numpy.get_include())
+
+
+pycocotools_mask = Extension(
+    'pycocotools._mask',
+    sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
+    include_dirs=['../common'],
+    extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+)
 
 setup(
     name='pycocotools',
     packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
+    package_dir={'pycocotools': 'pycocotools'},
+    # run-time dependencies
     install_requires=[
         'setuptools>=18.0',
+        'numpy',
+        'matplotlib>=2.1.0',
+    ],
+    # build-time dependencies
+    setup_requires=[
         'cython>=0.27.3',
-        'matplotlib>=2.1.0'
+        'numpy',
     ],
     version='2.0',
-    ext_modules= ext_modules
+    ext_modules=[pycocotools_mask],
+    cmdclass={'build': MyBuild},
 )
